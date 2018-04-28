8a0f19725279d76745ac2bb88ef4d5e47d98e037
Mohamed Mostafa <mohamedmostafa1505@gmail.com>
2018-04-28 21:14:43 +0200

    Revert "removed unnecessary files"
    This reverts commit ee7b3be59dc37e0989a8f95f0fb68fa30b8026e1.

---
+++ ChatApp/src/chatapp/ChatApplicaton.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.*;  
+import java.net.*; 
+/**
+ *
+ * @author ASUS
+ */
+public class ChatApplicaton extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+       try{      
+Socket s=new Socket("localhost",3001);  
+DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+dout.writeUTF("Hello Server");  
+dout.flush();  
+dout.close();  
+s.close();  
+
+}catch(Exception e){System.out.println(e);
+}  
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
