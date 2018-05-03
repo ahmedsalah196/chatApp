35f0f4022babdc715b733b1650c7649fae66999f
KarimM7mad <KarimMohamed25595@gmail.com>
2018-05-03 14:29:34 +0200

    chat room modified to kick users and different overloading of functions made for usage, also check code comments

--- ChatServer/src/chatserver/chatRoom.java
+++ ChatServer/src/chatserver/chatRoom.java
@@ -6,20 +6,43 @@ import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+class threadedMsgSender implements Runnable {
+
+    private String targetIP, message;
+    private DataOutputStream dout;
+    private DataInputStream din;
+
+    public threadedMsgSender(String targetIP, String message) {
+        this.targetIP = targetIP;
+        this.message = message;
+        this.din = null;
+        this.dout = null;
+    }
+
+    public void run() {
+        try {
+            Socket s = new Socket(this.targetIP, 3001);
+            this.dout = new DataOutputStream(s.getOutputStream());
+            this.dout.writeUTF(this.message);
+            //recieve acknowlogement lw 3aezeen
+        } catch (Exception ex) {
+            Logger.getLogger(chatRoom.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
 public class chatRoom {
 
     //lets assume clientsInRooms be arraylist of Strings(names or IPs) lsa m4 3aref 
-    public ArrayList<connection> clientsInRoom;
+    public ArrayList<user> clientsInRoom;
+    public user adminUser;
     public String roomName;
     public static int roomNumberTotake = 1;
     public static int portNumbersTotake = 20000;
     public ServerSocket serverSocket;
     public int currentPortNum;
     public int currentRoomNum;
-    public DataInputStream roomDataInputStream;
-    public DataOutputStream roomDataOutputStream;
     public ArrayList<String> messages;
-    
 
     public chatRoom() {
         this.currentPortNum = chatRoom.portNumbersTotake++;
@@ -27,8 +50,6 @@ public class chatRoom {
         this.roomName = "room." + (this.currentRoomNum);
         this.clientsInRoom = new ArrayList();
         this.messages = new ArrayList<>();
-        this.roomDataInputStream=null;
-        this.roomDataOutputStream=null;
         try {
             this.serverSocket = new ServerSocket(this.currentPortNum);
         } catch (Exception ex) {
@@ -36,55 +57,103 @@ public class chatRoom {
         }
     }
 
-    public void addToRoom(connection s) {
-        //client Call it to Join the chat by adding his socket to the arraylist  
-        this.clientsInRoom.add(s);
-    }
-
-    public void addToRoom(Socket clientSocket, String userName) {
-        this.clientsInRoom.add(new connection(clientSocket, userName));
-    }
-
-    //send msg to all other clients in the room 
-    public void sendMsgToAll(String msg) {
-        System.out.println("Message = "+msg);
-        int size = this.clientsInRoom.size();
-        this.messages.add(msg);
-        //change for loop to threading process
-        for (int i = 0; i < size; i++) {
-            try {
-                //send message 
-                connection tmp = (connection)this.clientsInRoom.get(i);
-                this.roomDataOutputStream = new DataOutputStream(tmp.s.getOutputStream());
-                this.roomDataOutputStream.writeUTF(msg+"=> socket:"+tmp.username);
-                //recieve acknowlogement
-            } 
-            catch (Exception ex) {
-                Logger.getLogger(chatRoom.class.getName()).log(Level.SEVERE, null, ex);
-            }
+    /* 
+       addToRoom => used to make a user Join the room 
+       parameters=> userClient : indicate the desired client to join the room
+                    sameORcopy : indicate whehter for the same user reference only (copy reference)
+                                  or for a new instance of the newClient Joined ( copy values in a new instance)
+     */
+    public void addToRoom(user newClient, boolean sameORcopy) {
+        //sameORcopy = true, for the same user reference only (copy reference)
+        //sameORcopy = false, for a new instance of the newClient Joined ( copy values )
+        if (sameORcopy) {
+            //add the reference of the user to the arrayList
+            this.clientsInRoom.add(newClient);
+        } else {
+            //add a copy of that user to the arraylist
+            this.clientsInRoom.add(new user(newClient.username, newClient.password, newClient.ip, newClient.status));
         }
     }
 
-    public static chatRoom createChatRoom(Socket s,String username){
+    /*
+      sendMsgToAll => used to send msg to all clients in the room
+      parameters => msg : message written in the text area in the GUI
+     */
+    public void sendMsgToAll(String msg) {
+        //format of message to send to each client
+        String msgToSend = "from =>" + "UserName" + "\n" + "Message => " + msg;
+        //get Number of Clients in the room
+        int size = this.clientsInRoom.size();
+        //create a thread for each client in the room and Start it's execution
+        Thread[] tharr = new Thread[size];
+        for (int i = 0; i < size; i++) {
+            tharr[i] = new Thread(new threadedMsgSender(this.clientsInRoom.get(i).ip, msgToSend));
+            tharr[i].start();
+        }
+        for (int i = 0; i < size; i++) {
+            try {
+                //wait for each thread to finish it's execution
+                tharr[i].join();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(chatRoom.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        //store it in the messages sent in this chatRoom
+        this.messages.add(msgToSend);
+    }
+
+    /*
+        createChatRoom => used to create a new Chat room 
+        parameters => username : string representing the user created the room
+                      password : string repesenting the password of that user account
+                      IP: string representing the IP of the user creating the room as it will required for transactions  
+     */
+    public static chatRoom createChatRoom(String username, String password, String IP) {
         chatRoom r = new chatRoom();
-        r.addToRoom(s, username);
+        //assign room admin
+        r.adminUser = new user(username, password, IP);
+        r.adminUser.adm = true;
+        //add that user to the room
+        r.addToRoom(r.adminUser, true);
         return r;
     }
 
-    @Override
-    public String toString() {
-        return "chatRoom{" +"Number of Clients="+this.clientsInRoom.size()+ "clientsInRoom=" + clientsInRoom + ", \nroomName=" + roomName + ", \nserverSocket=" + serverSocket + ", \ncurrentPortNum=" + currentPortNum + ", \ncurrentRoomNum=" + currentRoomNum + '}';
+    /*
+        kick any user by remove him from the arrayList
+        DISCLAIMER : must be used by the room admin only
+     */
+    public void kickAclient(String IPtoRemove){
+        for (int i = 0; i < this.clientsInRoom.size(); i++) {
+            if (this.clientsInRoom.get(i).ip.equalsIgnoreCase(IPtoRemove)) {
+                this.clientsInRoom.remove(i);
+                break;
+            }
+        }
     }
-    
-    
-    
+    /*
+        kick any user by remove him from the arrayList
+        DISCLAIMER : must be used by the room admin only
+     */
+    public void kickAclient(String IPtoRemove, String roomAdminUserName) {
+        if (roomAdminUserName.equalsIgnoreCase(this.adminUser.username)) {
+            for (int i = 0; i < this.clientsInRoom.size(); i++) {
+                if (this.clientsInRoom.get(i).ip.equalsIgnoreCase(IPtoRemove)) {
+                    this.clientsInRoom.remove(i);
+                    break;
+                }
+            }
+        }
+        
+    }
+
+    public String toString() {
+        return "chatRoom{" + "Number of Clients=" + this.clientsInRoom.size()
+                + "\nclientsInRoom=" + clientsInRoom
+                + ",\nroomName=" + roomName + ", "
+                + "\nserverSocket=" + serverSocket
+                + ",\ncurrentPortNum=" + currentPortNum
+                + ",\ncurrentRoomNum=" + currentRoomNum
+                + "\n}";
+    }
+
 }
-
-
-/* required from GUI
-1)function to process the client request
-
-
-
-
-*/
